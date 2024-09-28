1befc700beae1cba219a7c798d3e8f224f65f54a
CECILIA <202201001.ceciliadds@student.xavier.ac.in>
2024-09-29 05:08:42 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; 2024 SmartSpace Analytics Management System. All rights reserved.</p>
+    <p className="footer-creators">
+        Created by: Cecilia Dinesh, Amey Shelar, Kenneth Jezreel, Alvin Devassy
+      </p>
+  </footer>
+);
+
+export default Footer;
